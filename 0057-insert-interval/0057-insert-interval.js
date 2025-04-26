c0e271cb279ef70d88c14df98a21a70fc5eb6f39
Harsh Dave <harshdave1095@gmail.com>
2025-04-26 18:15:30 +0530

    Time: 194 ms (5.26%), Space: 67.2 MB (5.01%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function (intervals, newInterval) {
+    if (intervals.length === 0) return [newInterval];
+    // else if (newInterval[1] < intervals[0][0]) {
+    //     return [newInterval, ...intervals];
+    // } else if (newInterval[0] > intervals[intervals.length - 1][1]) {
+    //     return [...intervals, newInterval];
+    // } else {
+        const result = [];
+        // Iterate over intervals
+        for (let i = 0; i < intervals.length; i++) {
+            // If new interval's end is less than current interval start then return new interval and current intervals because intervals are sorted by starting point
+            if (newInterval[1] < intervals[i][0]) {
+                result.push(newInterval);
+                const remainingIntervals = intervals.slice(i);
+                return [...result, ...remainingIntervals];
+            }
+            // If new interval's start point is greater than current interval end then push current interval to result array
+            else if (newInterval[0] > intervals[i][1]) {
+                result.push(intervals[i]);
+            }
+            // Otherwise interval must be overlapping
+            else {
+                // Update new interval start and end with minimum from current interval start and new interval start and maximum from current interval end and new interval end
+                newInterval = [
+                    Math.min(newInterval[0], intervals[i][0]),
+                    Math.max(newInterval[1], intervals[i][1])
+                ];
+            }
+            console.log({ current: intervals[i], newInterval, result });
+        }
+        // Insert new interval at end in case if statement in the for loop is not executed and then return result
+        result.push(newInterval);
+        return result;
+    // }
+};

1a6b4e8eb24181ea02ddf263a20498005a2f3d59
Harsh Dave <harshdave1095@gmail.com>
2025-04-27 06:49:27 +0530

    Time: 6 ms (71.79%), Space: 55.5 MB (97%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -6,13 +6,13 @@ var lengthOfLongestSubstring = function(s) {
     const charList = [];
     let length = 0;
     let left = 0;
-    for (i = 0; i < s.length; i++) {
-        while (charList.includes(s[i])) {
+    for (let right = 0; right < s.length; right++) {
+        while (charList.includes(s[right])) {
             charList.shift();
             left += 1;
         }
-        charList.push(s[i]);
-        length = Math.max(length, i - left + 1);
+        charList.push(s[right]);
+        length = Math.max(length, right - left + 1);
     }
     return length;
 };

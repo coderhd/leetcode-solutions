28678b1a1a9ca9ac9cca10e4c4eaf1f8e4703ab0
Harsh Dave <harshdave1095@gmail.com>
2025-05-26 10:15:49 +0530

    Time: 5 ms (50%), Space: 58.5 MB (5.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,31 +3,12 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
-    // This will only work for sorted array
-    // Time Complexity: n
-    // let left = 0;
-    // let right = nums.length - 1;
-    // while (left < right) {
-    //     const sum = nums[left] + nums[right];
-    //     if (sum === target) return [left, right];
-    //     else if (sum < target) left++;
-    //     else right--;
-    // }
-
-    // return [-1, -1];
-
-// This will work for any array
-// Time Complexity n because 2n is n only in terms of Time Complexity
-    const map = new Map();
-    for (let i =0; i < nums.length; i++) {
-        map.set(nums[i], i);
-    }
+var twoSum = function(nums, target) {
+    const numObj = {};
+    nums.forEach((num, idx) => {
+        numObj[num] = idx;
+    });
     for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-        if (map.has(complement) && map.get(complement) !== i) {
-            return [i, map.get(complement)];
-        }
+        if (numObj[target - nums[i]] && numObj[target - nums[i]] !== i) return [i, numObj[target - nums[i]]];
     }
-    return [];
 };

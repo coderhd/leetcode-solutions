6b8a542b15628103cbda1642dc0f5c1fea5d868f
Harsh Dave <harshdave1095@gmail.com>
2026-03-19 14:48:27 +0530

    Time: 15 ms (80.22%), Space: 73.7 MB (70.32%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -5,7 +5,7 @@
 var containsDuplicate = function(nums) {
     const numSet = new Set();
     for(let i = 0; i < nums.length; i++) {
-        if (Boolean(numSet.has(nums[i]))) {
+        if (numSet.has(nums[i])) {
             return true;
         }
         else {

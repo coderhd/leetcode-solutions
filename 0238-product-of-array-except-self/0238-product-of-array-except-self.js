250abf9580447fcff2ec8431ff5e8f61a97a8bca
Harsh Dave <harshdave1095@gmail.com>
2026-03-20 16:30:11 +0530

    Time: 5 ms (77.13%), Space: 72.3 MB (26.78%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,25 +3,16 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    // Initialise results with value 1
-    const result = Array(nums.length).fill(1);
-    // initialise prefix with 1
+    const res = new Array(nums.length).fill(1);
     let prefix = 1;
-    // inerate once forward
-    for (let i = 0; i < nums.length; i++) {
-        // multiply result with prefix
-        result[i] = prefix * result[i];
-        // multiply prefix with current number
+    let suffix = 1;
+    for(let i = 0; i < nums.length; i++) {
+        res[i] = prefix;
         prefix *= nums[i];
     }
-    // initialise postfix with 1
-    let postfix = 1;
-    // iterate over numbers in reverse
-    for (j = nums.length - 1; j >= 0; j--) {
-        // multiply result with postfix
-        result[j] = postfix * result[j];
-        // multiply postfix with current number
-        postfix *= nums[j];
+    for(let j = nums.length - 1; j >= 0; j--) {
+        res[j] *= suffix;
+        suffix *= nums[j];
     }
-    return result;
+    return res;
 };

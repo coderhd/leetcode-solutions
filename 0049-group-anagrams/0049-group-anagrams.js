69079e160ef3f915b40d36e925c43f6aa34511fc
Harsh Dave <harshdave1095@gmail.com>
2026-03-22 00:19:37 +0530

    Time: 48 ms (15.06%), Space: 66.8 MB (82.43%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -5,9 +5,9 @@
 var groupAnagrams = function(strs) {
     const res = {};
     for (let s of strs) {
-        const count = Array.from({length: 26}, () => [0]);
+        const count = new Array(26).fill(0);
         for (let i = 0; i < s.length; i++) {
-            count[s.charCodeAt(i) - 'a'.charCodeAt(0)][0] += 1;
+            count[s.charCodeAt(i) - 'a'.charCodeAt(0)] += 1;
         }
         res[count.toString()] = res[count.toString()] ? [ ...res[count.toString()], s]: [s];
     }

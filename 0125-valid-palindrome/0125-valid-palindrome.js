ab45b7d562e8266fba12c0e7840eac332a53e6ed
Harsh Dave <harshdave1095@gmail.com>
2026-03-21 12:25:05 +0530

    Time: 4 ms (76.74%), Space: 55.7 MB (95.81%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,17 +3,11 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    const alNumStr = s.toLowerCase().trim().replace(/[^a-zA-Z0-9]/g, '');
-    let left = 0;
-    let right = alNumStr.length - 1;
-
-    while (left <= right) {
-        if (alNumStr[left] === alNumStr[right]) {
-            left++;
-            right--;
-        } else {
-            return false;
-        }
+    const pStr = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    for (let i = 0; i < pStr.length; i++) {
+        const currChar = pStr.charAt(i);
+        const compChar = pStr.charAt(pStr.length - 1 - i);
+        if (currChar !== compChar) return false;
     }
     return true;
 };

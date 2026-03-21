6f68bbe6cf5459ea611051464bc60a337d1c9e8f
Harsh Dave <harshdave1095@gmail.com>
2026-03-21 12:09:07 +0530

    Time: 12 ms (70.16%), Space: 55.4 MB (79.31%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,20 +3,28 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
+var isAnagram = function (s, t) {
     if (s.length !== t.length) return false;
-    sMap = new Map();
-    tMap = new Map();
-    for (let c of s) {
-        if (sMap.has(c)) sMap.set(c, sMap.get(c) + 1);
-        else sMap.set(c, 1);
+    const sMap = new Map();
+    const tMap = new Map();
+    for (let i = 0; i < s.length; i++) {
+        let charCount = sMap.get(s.charAt(i));
+        if (charCount) {
+            sMap.set(s.charAt(i), charCount + 1);
+        } else {
+            sMap.set(s.charAt(i), 1);
+        }
     }
-    for (let c of t) {
-        if (tMap.has(c)) tMap.set(c, tMap.get(c) + 1);
-        else tMap.set(c, 1);
+    for (let j = 0; j < t.length; j++) {
+        let charCount = tMap.get(t.charAt(j));
+        if (charCount) {
+            tMap.set(t.charAt(j), charCount + 1);
+        } else {
+            tMap.set(t.charAt(j), 1);
+        }
     }
-    for (let c of s) {
-        if (sMap.get(c) !== tMap.get(c)) return false;
+    for (let k = 0; k < s.length; k++) {
+        if (sMap.get(s.charAt(k)) !== tMap.get(s.charAt(k))) return false;
     }
     return true;
 };

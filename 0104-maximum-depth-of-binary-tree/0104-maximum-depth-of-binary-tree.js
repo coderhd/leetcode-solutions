250e62c8ccbf9aa0dae8ad3c82fa2cd946f78578
Harsh Dave <harshdave1095@gmail.com>
2026-03-22 12:34:25 +0530

    Time: 8 ms (4.22%), Space: 62.1 MB (5.02%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,6 +11,40 @@
  * @return {number}
  */
 var maxDepth = function(root) {
+    /* DFS Recursive
+
     if (!root) return 0;
     return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+    */
+
+    // DFS Iterative
+    
+    let res = 0;
+    let stack = [[root, 1]];
+
+    while(stack.length > 0) {
+        const [node, depth] = stack.pop();
+        if(node) {
+            res = Math.max(res, depth);
+            stack.push([node.left, depth + 1]);
+            stack.push([node.right, depth + 1]);
+        }
+    }
+    return res;
+    
+
+    // BFS
+    // if (!root) return 0;
+    // let level = 0;
+    // const queue = [root];
+    // while(queue.length > 0) {
+    //     for (let i = 0; i < queue.length; i++) {
+    //         const node = queue.shift();
+    //         if (node.left) queue.push(node.left);
+    //         if (node.right) queue.push(node.right);
+    //     }
+    //     console.log('q:', queue, level);
+    //     level += 1;
+    // }
+    // return level;
 };

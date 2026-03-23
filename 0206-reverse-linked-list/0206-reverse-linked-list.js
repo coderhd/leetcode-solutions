e79cfa477cec119de9eb47b0f7299798b1525d49
Harsh Dave <harshdave1095@gmail.com>
2026-03-23 10:24:47 +0530

    Time: 0 ms (100%), Space: 57.4 MB (69.83%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -10,26 +10,13 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    // Iterative Way
-    let prev = null;
     let curr = head;
-    while(curr) {
+    let prev = null;
+    while (curr) {
         const next = curr.next;
         curr.next = prev;
         prev = curr;
         curr = next;
     }
     return prev;
-
-    // Recursive Way
-    // if (!head) return null;
-
-    // let newHead = head;
-
-    // if (head.next) {
-    //     newHead = reverseList(head.next);
-    //     head.next.next = head;
-    // }
-    // head.next = null;
-    // return newHead;
 };
